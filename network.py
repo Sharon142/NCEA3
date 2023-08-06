b76a5bcdb5788f5c28f7d3097083096921547eb1
SharonCulas <culas.s@maristcollege.school.nz>
2023-08-07 10:45:52 +1200

    creating key for password manager

---
+++ network.py
@@ -0,0 +1,16 @@
+from cryptography.fernet import Fernet
+
+
+class PasswordManager:
+
+    def __init__(self) -> None:
+        self.key = None
+        self.password_file = None
+        self.password_dict = {}
+
+    def create_key(self , path):  
+        self.key = Fernet.generate_key()
+        print(self.key)  
+
+pm = PasswordManager()
+pm.create_key()
