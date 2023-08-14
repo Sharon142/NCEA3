bc88c2279131d454d1824b476c40145332cf2bfd
SharonCulas <culas.s@maristcollege.school.nz>
2023-08-15 09:55:13 +1200

    testing password manager

--- network.py
+++ network.py
@@ -42,14 +42,58 @@ class PasswordManager:
 
     def get_password(self, site):
         return self.password_dict[site]
-    
+
 
 def main():
     password = {
         "email": "1234567",
         "instagram": "myigpassword",
         "youtube": "helloworld123",
-        
-
-
+        "something": "myfavoritepassword_123"
     }
+
+    pm = PasswordManager()
+
+    print("""What do you want to do?
+    (1) Create a new key
+    (2) Load an existing key
+    (3) Create new password file
+    (4) Load existing password file
+    (5) Add a new password
+    (6) Get a password
+    (q) Quit                         
+    """)
+
+    done = False
+
+    while not done:
+
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            path = input("Enter path: ")
+            pm.create_key(path)
+        elif choice == "2":
+            path = input("Enter path: ")
+            pm.load_key(path)
+        elif choice == "3":
+            path = input("Enter path: ")
+            pm.create_password_file(path, password)
+        elif choice == "4":
+            path = input("Enter path: ")
+            pm.load_password_file(path)
+        elif choice == "5":
+            site = input("Enter the site: ")
+            password = input("Enter the password: ")
+            pm.add_password(site, password)
+        elif choice == "6":
+            site = input("What site do you want: ")
+            print(f"Password for {site} is {pm.get_password(site)}")
+        elif choice == "q":
+            done = True
+            print("Bye")
+        else:
+            print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    main()
